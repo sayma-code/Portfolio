ddaecaa004ddd405697e36c9e935c112c14a6bc1
sayma-code <saymaobaida2@gmail.com>
2023-10-27 08:11:57 +0600

    Updates

---
+++ precache-manifest.fb93910bd5579c73233bf11a08cdcec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "622d30f4f17e95fc547d",
+    "url": "/Portfolio/static/css/main.498184b2.chunk.css"
+  },
+  {
+    "revision": "622d30f4f17e95fc547d",
+    "url": "/Portfolio/static/js/main.622d30f4.chunk.js"
+  },
+  {
+    "revision": "73c4a386b51143b368b7",
+    "url": "/Portfolio/static/js/1.73c4a386.chunk.js"
+  },
+  {
+    "revision": "484317f60bbd62819c2b",
+    "url": "/Portfolio/static/css/2.632ef4e8.chunk.css"
+  },
+  {
+    "revision": "484317f60bbd62819c2b",
+    "url": "/Portfolio/static/js/2.484317f6.chunk.js"
+  },
+  {
+    "revision": "4539174da66b4300764b",
+    "url": "/Portfolio/static/js/runtime~main.4539174d.js"
+  },
+  {
+    "revision": "dd7980bfa390ad5e0cc0c1b4079a38c8",
+    "url": "/Portfolio/static/media/contact-background.dd7980bf.png"
+  },
+  {
+    "revision": "1b02ba3b542047023c3338b7df1bf694",
+    "url": "/Portfolio/index.html"
+  }
+];
