7a3622056e127fcd1bb77d7073cbd4551f63e281
sayma-code <saymaobaida2@gmail.com>
2026-02-19 03:51:15 +0200

    Updates

---
+++ precache-manifest.a3b2a2fc1d5e90937c89f60a2b1859bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cda16288162261ee5284f2404010f8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e1c3dd95b1ae3cc2b36a",
+    "url": "/Portfolio/static/css/2.92889dfb.chunk.css"
+  },
+  {
+    "revision": "62a644d507a1c5b12ac0",
+    "url": "/Portfolio/static/css/main.e1edda29.chunk.css"
+  },
+  {
+    "revision": "e1c3dd95b1ae3cc2b36a",
+    "url": "/Portfolio/static/js/2.517a420f.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/Portfolio/static/js/2.517a420f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eace4a913192984f096",
+    "url": "/Portfolio/static/js/3.b6a61616.chunk.js"
+  },
+  {
+    "revision": "62a644d507a1c5b12ac0",
+    "url": "/Portfolio/static/js/main.82473ff6.chunk.js"
+  },
+  {
+    "revision": "4e41c8bbb521687be4a7",
+    "url": "/Portfolio/static/js/runtime-main.ad765fd0.js"
+  },
+  {
+    "revision": "9318bc5b23512d9fb6fd2e69c2d5ae3a",
+    "url": "/Portfolio/static/media/Consultancy.9318bc5b.png"
+  },
+  {
+    "revision": "e85d9b58a1472f0a82b1da0186de387e",
+    "url": "/Portfolio/static/media/OMS.e85d9b58.png"
+  },
+  {
+    "revision": "f599be693562ddffa024e0a247c2370b",
+    "url": "/Portfolio/static/media/about.f599be69.png"
+  },
+  {
+    "revision": "5cf97aeb0727f1089438bb39e17f645f",
+    "url": "/Portfolio/static/media/ballon.5cf97aeb.png"
+  },
+  {
+    "revision": "fb0f6887f1e56991102c517bf6768a77",
+    "url": "/Portfolio/static/media/discord_bot.fb0f6887.png"
+  },
+  {
+    "revision": "0cc28002e98a76780ad7add3bdd0d874",
+    "url": "/Portfolio/static/media/portfolio-bottom.0cc28002.png"
+  },
+  {
+    "revision": "c1270eb9b7e0c27a9117e0c50e7d4262",
+    "url": "/Portfolio/static/media/spt.c1270eb9.png"
+  }
+]);
